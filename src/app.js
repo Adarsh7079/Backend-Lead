26ecab55610f6dde4e6881a508a74a8c70a90b5d
adarsh7079 <adarshk8271@gmail.com>
2025-03-11 10:41:47 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app= express()
 // Cors for proving Frontend URL to connect backend with frontend
 app.use(cors({
-    origin: 'http://localhost:5173', // Specify the allowed origin
+    origin: '*', // Specify the allowed origin
     credentials: true, // Allow credentials (cookies) for login signup and local storage for Authenticationa and Authorization
 }));
 app.use(express.json({limit:'16kb'}))
